d6c22323f50bc35faebfaad88fb460c5712027bb
Sirio <sirioroberto1@hotmail.com>
2022-10-05 08:48:05 -0300

    Category

---
+++ src/main/java/com/sirius/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.sirius.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sirius.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
